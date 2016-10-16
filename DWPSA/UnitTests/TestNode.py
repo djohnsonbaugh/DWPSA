3d7b77d3bf43e570abc37bdd9f53918f2ca49998
DeWayne Johnsonbaugh <djohnsonbaugh@gmail.com>
2016-10-15 23:38:45 -0400

    Initial fill out of unit tests

--- DWPSA/UnitTests/TestNode.py
+++ DWPSA/UnitTests/TestNode.py
@@ -1,8 +1,23 @@
 import unittest
-import dwNode
+from dwNode import Node
 
 class TestNode(unittest.TestCase):
     """Unit Test class for Node"""
+
+    def test_Constructor(self):
+        stname = "station"
+        kv = "115"
+        ndid = "AB"
+        company = "ABC"
+
+        nd = Node(stname, kv, ndid, company)
+
+        self.assertEqual(nd.ID, (stname, kv, ndid))
+        self.assertEqual(nd.StationID, stname)
+        self.assertEqual(nd.Voltage, kv)
+        self.assertEqual(nd.Name, ndid)
+        self.assertEqual(nd.Company, company)
+        return
 
 
 if __name__ == '__main__':
--- DWPSA/UnitTests/TestStation.py
+++ DWPSA/UnitTests/TestStation.py
@@ -1,8 +1,34 @@
 import unittest
+from dwStation import *
+from dwNode import *
 
 class TestStation(unittest.TestCase):
-    def test_A(self):
-        self.fail("Not implemented")
+    def test_ConstructorA(self):
+        stname = "station"
+
+        s = Station(stname)
+
+        self.assertEqual(s.ID, stname)
+        return
+
+
+    def test_AddNode(self):
+        stname = "station"
+        kv = "115"
+        ndid = "AB"
+        company = "ABC"
+
+        nd = Node(stname, kv, ndid, company)
+        s = Station(stname)
+
+        s.AddNode(nd)
+        #checks if Station contains nodes
+        self.assertEqual(s.Nodes[(stname, kv, ndid)].Name, ndid)
+        
+        #checks if Node knows what Station it is at.
+        self.assertEqual(nd.Station.ID, stname)
+        return
+
 
 if __name__ == '__main__':
     unittest.main()
